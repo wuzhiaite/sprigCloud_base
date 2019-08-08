949cd824100a6dbee7be552c160da066ab2aa99c
wuzhiaite <656494298@qq.com>
2019-08-08 23:56:23 +0800

    新增使用eureka

--- microservicecloud/microserviceclodproviderdept8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_APP.java
+++ microservicecloud/microserviceclodproviderdept8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_APP.java
@@ -2,10 +2,12 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class DeptProvider8081_APP {
+@EnableEurekaClient
+public class DeptProvider8001_APP {
     public static void main(String[] args){
-        SpringApplication.run(DeptProvider8081_APP.class,args);
+        SpringApplication.run(DeptProvider8001_APP.class,args);
     }
 }
